3455985da28be39081618df44fc04211df2dd12b
CrazyHuman00 <asakurashi00@icloud.com>
2023-09-18 14:21:41 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import json
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import TextSendMessage
+
+file = open("info.json", "r")
+info = json.load(file)
+
+CHANNEL_ACCESS_TOKEN = info["CHANNEL_ACCESS_TOKEN"]
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+
+def main():
+    USER_ID = info["USER_ID"]
+    messages = TextSendMessage(text="おはよう〜\n朝だよ〜おきて〜")
+    line_bot_api.push_message(USER_ID, messages=messages)
+
+if __name__ == "__main__":
+    main()
+
